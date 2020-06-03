efc58a53279fa0f65edb207289810d3ebfd4e3b0
Robby <r0bby@gmx.net>
2020-06-04 00:32:05 +0200

    Modes for renderer

---
+++ src/main/java/org/burgerbude/labymod/addons/pingtag/render/DisplayMode.java
@@ -0,0 +1,15 @@
+package org.burgerbude.labymod.addons.pingtag.render;
+
+/**
+ * <b>DisplayMode</b> is representing the display modes of the renders
+ *
+ * @author Robby
+ */
+public enum DisplayMode {
+
+    ABOVE_HEAD,
+    LINE,
+    BESIDE_LEFT_BODY,
+    BESIDE_RIGHT_BODY
+
+}
---
+++ src/main/java/org/burgerbude/labymod/addons/pingtag/render/RainbowMode.java
@@ -0,0 +1,13 @@
+package org.burgerbude.labymod.addons.pingtag.render;
+
+/**
+ * <b>RainbowMode</b> is representing the rainbow modes for the render
+ *
+ * @author Robby
+ */
+public enum RainbowMode {
+
+    NORMAL,
+    WAVE
+
+}
