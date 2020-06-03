082cf07567aeebfc482afe74903142ee64e26faf
Robby <r0bby@gmx.net>
2020-06-04 00:04:46 +0200

    Addon setup

---
+++ src/main/java/org/burgerbude/labymod/addons/pingtag/PingTagAddon.java
@@ -0,0 +1,28 @@
+package org.burgerbude.labymod.addons.pingtag;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+import java.util.List;
+
+/**
+ * The main class of the addon
+ *
+ * @author Robby
+ */
+public class PingTagAddon extends LabyModAddon {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+}
